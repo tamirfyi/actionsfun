ca8f45bacd38414bc4ad024b12fdb194bf2a3ad9
Tamir <tmelzerb@gmail.com>
2024-05-28 17:03:51 -0700

    testing build job for demo, passing

--- nest-api/test/app.e2e-spec.ts
+++ nest-api/test/app.e2e-spec.ts
@@ -16,11 +16,10 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return (
-      request(app.getHttpServer())
-        .get('/')
-        // .expect('Hello World!');
-        .expect('This should not pass')
-    );
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+    // .expect('This should not pass')
   });
 });
